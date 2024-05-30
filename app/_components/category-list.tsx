8dbb012df255d5b6ce320a2a6d292498f54b7c04
Danilo Gomes <51554360+dantls@users.noreply.github.com>
2024-05-30 22:02:44 +1000

    style: correct the padding of category list

--- app/_components/category-list.tsx
+++ app/_components/category-list.tsx
@@ -5,7 +5,7 @@ const CategoryList = async () => {
   const categories = await db.category.findMany({});
 
   return (
-    <div className="flex gap-3 overflow-x-scroll px-5 py-2 [-ms-overflow-style:none] [scrollbar-width:none] [`&::-webkit-scrollbar`]:hidden">
+    <div className="flex gap-3 overflow-x-scroll py-2 [-ms-overflow-style:none] [scrollbar-width:none] [`&::-webkit-scrollbar`]:hidden">
       {categories.map((category) => (
         <CategoryItem key={category.id} category={category} />
       ))}
