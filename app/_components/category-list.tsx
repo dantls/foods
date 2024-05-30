da9cbb03372b4b2f0f1d8abae8ca97a6a3c42268
Danilo Gomes <51554360+dantls@users.noreply.github.com>
2024-05-30 21:26:03 +1000

    fix: correct category list

--- app/_components/category-list.tsx
+++ app/_components/category-list.tsx
@@ -5,7 +5,7 @@ const CategoryList = async () => {
   const categories = await db.category.findMany({});
 
   return (
-    <div className="grid grid-cols-2 gap-3">
+    <div className="flex gap-3 overflow-x-scroll px-5 py-2 [-ms-overflow-style:none] [scrollbar-width:none] [`&::-webkit-scrollbar`]:hidden">
       {categories.map((category) => (
         <CategoryItem key={category.id} category={category} />
       ))}
